9390b95880a8882bc20d70689ed6a893043b35c8
Ubuntu <ubuntu@ip-172-31-24-138.us-west-2.compute.internal>
2016-02-15 22:31:44 +0000

    working search

--- search.py
+++ search.py
@@ -1,30 +1,41 @@
 import sys 
-import gensim
+from json import loads
+import operator
+import re
 
-model = gensim.models.Word2Vec()
-sentences = gensim.models.word2vec.LineSentence('/data/text8')
-print 'sentences loaded'
-model.build_vocab(sentences)
-print 'vocab built'
-model.train(sentences)
-print 'model trained'
-model.save('text8_model')
+#import gensim
+
+#model = gensim.models.Word2Vec()
+#sentences = gensim.models.word2vec.LineSentence('/data/text8')
+#print 'sentences loaded'
+#model.build_vocab(sentences)
+#print 'vocab built'
+#model.train(sentences)
+#print 'model trained'
+#model.save('text8_model')
 
 #model = gensim.models.Word2Vec.load('/data/gensim_wiki_model')
 
-print model.most_similar(positive=['woman', 'king'], negative=['man'])
-print model.most_similar(positive=['banana', 'bread'])
+#print model.most_similar(positive=['woman', 'king'], negative=['man'])
+#print model.most_similar(positive=['banana', 'bread'])
 
 def get_recipes(search, db):
-    pass
-    # for recipe in db:
-        # compute match_score(search, recipe)
-    # sort scores and return
+    with open(db) as f:
+        recipes = []
+        for line in f.readlines():
+            recipe = loads(line)
+            score = compute_match(search, recipe)
+            if score > 0:
+                recipes.append((recipe, score))
+    sorted_r = sorted(recipes, key=operator.itemgetter(1))
+    sorted_r.reverse()
+    for r in sorted_r:
+        print r[0]['name']
 
 def compute_match(search, recipe):
-    pass
-    # compute word2vec similarity between search the top N keywords in recipe
-    # return similarity score
+    p = re.compile('('+search.lower()+')')
+    result = p.findall((recipe['name'] + recipe['description']).lower()) 
+    return len(result)
 
 if __name__ == '__main__':
     # arguments are search string and db file
