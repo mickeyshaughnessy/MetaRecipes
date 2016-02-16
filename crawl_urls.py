8b8b9dd3379d051fb30831217dfe965daa0b9929
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2016-02-15 20:26:28 -0800

    add basic crawler to get urls

---
+++ crawl_urls.py
@@ -0,0 +1,30 @@
+# This script starts at a root and crawls through the links.
+# It collects and moves on to all links containing urls like:
+# "...allrecipes.com/recipe..."
+
+import requests
+import sys
+import re
+from gevent import sleep
+from random import shuffle, uniform
+
+seen = []
+p = re.compile('"http://allrecipes\.com/recipe.*?"')
+
+def crawl(url):
+    print url
+    #sleep(uniform(0.2,0.8))
+    seen.append(url)
+    r = requests.get(url)
+    matches = p.findall(r.text)
+    shuffle(matches) 
+    for m in matches:
+        m = m.replace('"','')
+        if m not in seen: 
+            with open('all_urls.txt', 'a') as f:
+                f.write(m+'\n')
+            crawl(m)
+
+if __name__ == "__main__":
+    root = sys.argv[1]
+    crawl(root)
