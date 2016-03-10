d5f8dfce95408d6600e9aef0a365c2f8933d8a58
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2016-03-10 05:47:09 -0800

    backed out filter code in search

--- search.py
+++ search.py
@@ -45,39 +45,40 @@ def try_similarity(w1, w2):
         return 0
  
 def compute_match(search, recipe):
+    #print search
+    
     pall = re.compile('('+search.lower()+')') #pall is the regex for the whole search string
     ps = [re.compile('('+s.lower()+' )') for s in search.split(' ')] #ps is the list of regexs for each word.
     rname = recipe['name'].lower()
-    print [p.findall(' chili') for p in ps] 
+    #print [p.findall('chili') for p in ps] 
     
-    recipe_2grams = [' '.join(r) for r in zip(*[rname.split(' ')[i:] for i in range(2)])]  
-    recipe_2grams += ['-'.join(r) for r in zip(*[rname.split(' ')[i:] for i in range(2)])]  
-    search_2grams = [' '.join(s) for s in zip(*[search.lower().split(' ')[i:] for i in range(2)])]
-    search_2grams += ['-'.join(s) for s in zip(*[search.lower().split(' ')[i:] for i in range(2)])]
-    
-    filters = [
-            'vegan', # no meat, cheese, egg, or animal-derived ingredients 
-            'vegetarian', # no meat
-            'low fat', 'low-fat', # not a lot of saturated fat
-            'slow cooker', 'slow-cooker', # uses a slow-cooker or crockpot
-            'gluten', 'gluten-free', 'gluten free', # no gluten
-            'paleo', # high protein, low carb
-            'raw', # no cooking
-            'easy', 'quick', # low cook time, few ingredients
-            'low-sodium' # no high salt ingredients
-            ]
-    _filters = set(filters) & \
-               (set([s.lower() for s in search.split(' ')]) | set(search_2grams)) & \
-               (set(rname.split(' ')) | set(recipe_2grams))
-    if not _filters:
-        return 0 # if the filter isn't in the name, don't return the recipe(?)
+    #recipe_2grams = [' '.join(r) for r in zip(*[rname.split(' ')[i:] for i in range(2)])]  
+    #recipe_2grams += ['-'.join(r) for r in zip(*[rname.split(' ')[i:] for i in range(2)])]  
+    #search_2grams = [' '.join(s) for s in zip(*[search.lower().split(' ')[i:] for i in range(2)])]
+    #search_2grams += ['-'.join(s) for s in zip(*[search.lower().split(' ')[i:] for i in range(2)])]
+    #
+    #filters = [
+    #        'vegan', # no meat, cheese, egg, or animal-derived ingredients 
+    #        'vegetarian', # no meat
+    #        'low fat', 'low-fat', # not a lot of saturated fat
+    #        'slow cooker', 'slow-cooker', # uses a slow-cooker or crockpot
+    #        'gluten', 'gluten-free', 'gluten free', # no gluten
+    #        'paleo', # high protein, low carb
+    #        'raw', # no cooking
+    #        'easy', 'quick', # low cook time, few ingredients
+    #        'low-sodium' # no high salt ingredients
+    #        ]
+    #_filters = set(filters) & \
+    #           (set([s.lower() for s in search.split(' ')]) | set(search_2grams)) & \
+    #           (set(rname.split(' ')) | set(recipe_2grams))
+    #if not _filters:
+    #    return 0 # if the filter isn't in the name, don't return the recipe(?)
 
-    dashes = [' '.join(f.split('-')) for f in _filters if '-' in f]
-    if dashes: 
-        _filters.update([' '.join(dashes)])
-    for f in _filters:
-        rname = rname.replace(f, '')
-    print rname
+    #dashes = [' '.join(f.split('-')) for f in _filters if '-' in f]
+    #if dashes: 
+    #    _filters.update([' '.join(dashes)])
+    #for f in _filters:
+    #    rname = rname.replace(f, '')
     # scoring is this way:
         # One or more of the search terms must appear in the recipe title.
         # If both do, the score is doubled.
@@ -87,10 +88,10 @@ def compute_match(search, recipe):
         rbody = (recipe['description'] + ' ' + ' '.join(recipe['recipeInstructions'])).lower() 
     except:
         rbody = recipe['description'] 
-    #name_score = sum([len(p.findall(rname)) for p in ps])
-    print [p.findall(' chili') for p in ps] 
-    print name_score
-    raw_input() 
+    name_score = sum([len(p.findall(rname)) for p in ps])
+    #print [p.findall(' chili') for p in ps] 
+    #print name_score
+    #raw_input() 
     body_score = 0
     if len(pall.findall(rname)) > 0 and len(search.split(' ')) > 1:
         name_score = name_score * 2 #double name score is the whole search string is in the name
