2d43cacfefd28eae0e4a2073492f4b7328ae7a70
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2016-03-16 20:15:04 -0700

    fix app server url

--- test_server.py
+++ test_server.py
@@ -1,6 +1,7 @@
 import requests
 
-resp = requests.get('http://localhost:5010/recipes/')
+#resp = requests.get('http://localhost:5010/recipes/')
 #resp = requests.get('http://localhost:5010/metasearch/')
-resp = requests.get('http://localhost:5010/metasearch?qstring=applesauce')
+#resp = requests.get('http://localhost:5010/metasearch?qstring=applesauce')
+resp = requests.get('http://ec2-54-200-146-86.us-west-2.compute.amazonaws.com/metasearch?qstring=applesauce')
 print resp.text
